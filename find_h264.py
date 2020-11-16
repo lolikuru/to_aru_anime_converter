0bc0b4373f5aefa7cd66e8c1966b11d1eb61b431
pglazov <pglazov@mirantis.com>
2020-11-16 21:08:02 +0000

    check converted files

--- find_h264.py
+++ find_h264.py
@@ -3,6 +3,7 @@ import sys, subprocess, json, os
 import argparse
 
 def cutter(item_c):
+    print item_c
     while item_c.find('[') != -1:
         start = item_c.find('[')
         end = item_c.find(']')
@@ -49,8 +50,6 @@ print "Move find dir:"+dir_path
 for root, dirs, files in os.walk(dir_path): 
     for file in files:   
         if file.endswith('.mkv') | file.endswith('.mp4') | file.endswith('.avi'): 
-            full_root.append(str(root+'/'+str(file))) 
-            l_root.append(str(root))
             check = (str(root+'/'+str(file)))
 #           print check
             output = subprocess.check_output([
@@ -62,54 +61,35 @@ for root, dirs, files in os.walk(dir_path):
                 check
             ])[:-1]
             if  output == 'h264':
-#               print output
-#               print (str(file))
+                full_root.append(str(root+'/'+str(file)))
+                l_root.append(str(root))
                 cutter(str(file))
-                output = subprocess.check_output([
-                    "ffprobe",
-                    "-v", "error",
-                    "-print_format", "json",
-                    "-show_format",
-                    "-show_streams",
-                    check
-                ])
-            data = json.loads(output)
-            format, streams = [data[k] for k in ('format','streams')]
-
-            for s in streams:
-                if s['codec_type'] == 'video':
-                    print 'Stream #{} {}'.format(s['index'], s['codec_type'])
-                    print 'Codec: {}'.format(s['codec_name'])
-                    print 'Resolution: {}x{}'.format(
-                        s.get('width', '?'), s.get('height', '?'))
-                elif s['codec_type'] == 'audio':
-                    print 'Stream #{} {}'.format(s['index'], s['codec_type'])
-                    print 'Codec: {}'.format(s['codec_name'])
-                    print 'Sample Rate: {}'.format(s.get('sample_rate', '?'))
-count=0
+count = 0
 for i in l_fix:
-    print full_root[count]
-    print i
-    print full_root[count][0:-4] + '.ass'
-    if os.path.exists(full_root[count][0:-4] + '.ass'): 
+#    print full_root[count]
+#    print i
+    print 'cp ' + full_root[count][0:-4] + '.ass ' + l_root[count]+'/'+i[0:-4]+'.ass'
+    if os.path.exists(full_root[count][0:-4] + '.ass') & not os.path.exists(l_root[count]+'/'+i[0:-4]+'.ass'): 
         output = subprocess.check_output([
             "cp",
             full_root[count][0:-4]+'.ass',
-            str(l_root[count]+'/'+i[0:-4]+'.ass')
+            l_root[count]+'/'+i[0:-4]+'.ass'
         ])
-    process = "ffmpeg -i '" + full_root[count] + "' -c:v libx265 -x265-params crf=25 -c:a copy '" + str(l_root[count]+'/'+i) + "'"
-    p = subprocess.Popen([
-        "ffmpeg",
-        "-i", full_root[count],
-        "-c:v", "libx265",
-        "-x265-params", "crf=25",
-        "-c:a", "copy", 
-        str(l_root[count]+'/'+i)],
-        stderr=subprocess.PIPE)
-    chatter = p.stderr.read(1024)
-    while chatter.rstrip() != '':
+    
+    if not (os.path.exists(str(l_root[count]+'/'+i))):
+        process = "ffmpeg -i '" + full_root[count] + "' -c:v libx265 -x265-params crf=25 -c:a copy '" + str(l_root[count]+'/'+i) + "'"
+        p = subprocess.Popen([
+            "ffmpeg",
+            "-i", full_root[count],
+            "-c:v", "libx265",
+            "-x265-params", "crf=25",
+            "-c:a", "copy", 
+            str(l_root[count]+'/'+i)],
+            stderr=subprocess.PIPE)
         chatter = p.stderr.read(1024)
-        out = chatter.rstrip()
-        print(out)
+        while chatter.rstrip() != '':
+            chatter = p.stderr.read(1024)
+            out = chatter.rstrip()
+            print(out)
     count +=1
 
